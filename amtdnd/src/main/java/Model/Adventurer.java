202c1d95b494882be4718661d790b94672bc2949
Yoann0000 <yoann.rohrbasser@bluewin.ch>
2019-10-16 09:40:22 +0200

    added stats & Exp

--- amtdnd/src/main/java/Model/Adventurer.java
+++ amtdnd/src/main/java/Model/Adventurer.java
@@ -3,29 +3,45 @@ package Model;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
-@Builder
 @EqualsAndHashCode
 public class Adventurer {
 
-    public boolean setStat(String statName, int statNum) {
-        if (!stats.containsKey(statName)) {
-            return false;
-        } else {
-            stats.put(statName, statNum);
-            return true;
-        }
+    public Adventurer(int id, String name, Race race, CharacterClass klass) {
+        this.id = id;
+        this.name = name;
+        this.race = race;
+        this.klass = klass;
+        this.experience = 0;
+        this.gold = 0;
+        this.stats = new HashMap<Stats, Integer>();
+        stats.put(Stats.STR, 8);
+        stats.put(Stats.DEX, 8);
+        stats.put(Stats.CON, 8);
+        stats.put(Stats.INT, 8);
+        stats.put(Stats.WIS, 8);
+        stats.put(Stats.CHA, 8);
+    }
+
+    public void setStat(Stats statName, int statNum) {
+        stats.put(statName, statNum);
+    }
+
+    public int getLevel() {
+        return experience == 0 ? 1 : (int) Math.floor(Math.log10(experience));
     }
 
     private int id;
     private String name;
     private Race race;
     private CharacterClass klass;
-    private int level;
+    private int experience;
     private int gold;
-    private Map<String, Integer> stats;
+    private Map<Stats, Integer> stats;
 
     private enum CharacterClass {
         BARBARIAN("Barbarian"),
@@ -56,6 +72,17 @@ public class Adventurer {
         TIEFLING("Tiefling");
 
         Race(String name) {}
+    }
+
+    private enum Stats {
+        STR("Strength"),
+        DEX("Dexterity"),
+        CON("Constitution"),
+        INT("Intelligence"),
+        WIS("Wisdom"),
+        CHA("Charisma");
+
+        Stats(String name) {}
     }
 }
 
