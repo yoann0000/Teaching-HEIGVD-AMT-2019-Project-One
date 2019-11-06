9da1ec01914277bfe608bb77b3a85bc450dc6f00
yoann0000 <yoann.rohrbasser@bluewin.ch>
2019-11-06 10:00:40 +0100

    db access

--- amtdnd/src/main/java/Presentation/LoginServlet.java
+++ amtdnd/src/main/java/Presentation/LoginServlet.java
@@ -2,18 +2,26 @@ package Presentation;
 
 import business.AuthenticationService;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 @WebServlet(name="LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    private void doRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter pw = response.getWriter();
         response.setContentType("text/html");
 
@@ -22,12 +30,39 @@ public class LoginServlet extends HttpServlet {
 
         //ToDo Connexion a ce qui se connecte à la DB
         String pwdHash = "";
-        //ToDo Changer les valeurs
-        if(new AuthenticationService().checkPassword(password, pwdHash))
+        try {
+            Context context = new InitialContext();
+            DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/myDB");
+            Connection conn = dataSource.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT password FROM player WHERE name = user");
+            pwdHash = rs.toString();
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        if(new AuthenticationService().checkPassword(password, pwdHash)) {
             pw.println("Login Success...!");
-        else
+            request.getSession().setAttribute("principal", user);
+            response.sendRedirect("/index");
+        } else {
             pw.println("Login Failed...!");
+        }
         pw.close();
 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doRequest(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doRequest(request, response);
     }
 }
