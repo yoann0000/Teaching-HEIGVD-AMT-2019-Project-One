c860eca9865cb82061a2a52d7be61a7dd8c4c54d
yoann0000 <yoann.rohrbasser@bluewin.ch>
2019-11-06 10:25:38 +0100

    unused imports deleted

--- amtdnd/src/main/java/Presentation/LoginServlet.java
+++ amtdnd/src/main/java/Presentation/LoginServlet.java
@@ -4,21 +4,12 @@ import business.AuthenticationService;
 import datastore.exception.KeyNotFoundException;
 import integration.AdventurerDAO;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 
 @WebServlet(name="LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -29,8 +20,6 @@ public class LoginServlet extends HttpServlet {
 
         String user = request.getParameter("UserName");
         String password = request.getParameter("UserPsw");
-
-        //ToDo Connexion a ce qui se connecte à la DB
 
         String pwdHash = null;
         try {
